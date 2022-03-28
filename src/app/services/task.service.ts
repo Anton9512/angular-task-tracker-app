223005414ea3058298e9940187596395add25056
Anton9512 <anton.mail9512@gmail.com>
2022-03-28 14:28:42 +0300

    Http Client and fetching tasks

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { TASKS } from 'src/app/mock-tasks';
 import { Task } from 'src/app/Task';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
